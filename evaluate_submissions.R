8288cfb76fd7ccf69d62ec25b239e4c291914678
Ian Lundberg <ilundberg@cornell.edu>
2025-01-07 19:22:16 -0800

    Update evaluate_submissions

--- evaluate_submissions.R
+++ evaluate_submissions.R
@@ -2,7 +2,7 @@
 # Evaluate challenge exercise
 
 predictions <- read_csv(
-  "/users/ilundberg/downloads/Forecasting challenge_ Sex gap in pay (Responses) - Form Responses 1.csv",
+  "/users/ilundberg/downloads/Forecasting challenge_ Sex gap in pay (Responses) - Form Responses 1 (2).csv",
   col_names = c("time","name","sample_size","male","female"),
   skip = 1
 )
@@ -10,10 +10,15 @@ predictions <- read_csv(
 
 truth <- read_csv("assets/challenge_truth.csv")
 
-predictions |>
+results <- predictions |>
   pivot_longer(cols = c("male","female"), names_to = "sex", values_to = "yhat") |>
   left_join(truth, by = "sex") |>
   mutate(sq_error_log_scale = (log(yhat) - meanlog) ^ 2) |>
   select(name, sample_size, sex, prediction = yhat, truth = income, sq_error_log_scale) |>
-  group_by(sex) |>
   arrange(sq_error_log_scale)
+
+results |>
+  filter(sex == "male")
+
+results |>
+  filter(sex == "female")
